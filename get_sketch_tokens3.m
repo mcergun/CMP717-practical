3b9cdbb02813975b569f4c7076af5f5bf13855e0
Mert Can Ergün <mcergun@windowslive.com>
2017-03-05 17:17:47 +0300

    get_sketch_tokens3 semi working

--- get_sketch_tokens3.m
+++ get_sketch_tokens3.m
@@ -1,4 +1,4 @@
-function cur_gt = ... 
+function [img_features] = ... 
     get_sketch_tokens3(train_img_dir, train_gt_dir, feature_params)
 
 % 'img_features' is N x feature dimension. You probably want it to be
@@ -15,14 +15,11 @@ function cur_gt = ...
 train_imgs = dir( fullfile( train_img_dir, '*.jpg' ));
 % train_gts  = dir( fullfile( train_gt_dir,  '%.mat' )); %don't need to look them up, assume they exist for every image
 % num_imgs = length(train_imgs); % You don't need to sample them all while debugging.
-num_imgs = 30;
+num_imgs = 20;
 
 num_samples = 30000;
 pos_ratio   = 0.5; %The desired percentage of positive samples. 
 %It's not critical that your function find exactly this many samples.
-pos_samples = num_samples * pos_ratio;
-neg_samples = num_samples * (1 - pos_ratio);
-
 
 %14 channels
 %3 color
@@ -35,13 +32,13 @@ sketch_features = zeros(num_samples, daisy_feature_dims, 'single');
 
 %DELETE THIS PLACEHOLDER
 labels = ones(num_samples,1);
-img_features = rand(num_samples,40);
-img_features = single(img_features); %needs to be single precision
 
 % feature params 
-features_r = feature_params.CR;
+feat_r = feature_params.CR;
 daisy_r = feature_params.R;
-feature_size = 2 * features_r + 1;
+feat_sz = 2 * feat_r + 1;
+
+img_features = single(zeros(num_samples, feat_sz * feat_sz * 14));
 
 for i = 1:num_imgs
     fprintf(' Sampling patches / annotations from %s\n', train_imgs(i).name);
@@ -49,21 +46,60 @@ for i = 1:num_imgs
     cur_img = imread(fullfile(train_img_dir, train_imgs(i).name));
     cur_gt  = zeros(size(cur_img,1), size(cur_img,2));
         
-    dzy = compute_daisy(cur_img);
+    dsy = compute_daisy(cur_img);
     
     annotation_struct  = load(fullfile(train_gt_dir, [cur_name '.mat']));
     for j = 1:length(annotation_struct.groundTruth)
         cur_gt = cur_gt + annotation_struct.groundTruth{j}.Boundaries; 
     end
     
-    pos_index = find(cur_gt);
-    neg_index = find(cur_gt == 1);
+    [pos_rows, pos_cols] = find(cur_gt);
+    [neg_rows, neg_cols] = find(~cur_gt);
     
-    imshow(cur_gt);
-    waitforbuttonpress
+    cur_gt_sz = size(cur_gt);
+    
     % Pad the current image and then call 'channels = get_channels(cur_img)'
-    padded_img = im2double(imPad(cur_img, feature_params.CR, 'symmetric'));
+    padded_img = im2double(imPad(cur_img, feat_r, 'symmetric'));
     channels = get_channels(im2single(padded_img));
+    
+    % trying to prevent possible index out of bounds for small images
+    pos_samples = min(num_samples * pos_ratio, size(pos_rows, 1));
+    neg_samples = min(num_samples - pos_samples, size(neg_cols, 1));
+    fprintf(' Number of positive samples = %d, Number of negative samples = %d\n', ...
+        pos_samples, neg_samples);
+    
+    % shuffle indexes to get a more meaningful patch combination     
+    pos_index_shuffled = randperm(pos_samples, pos_samples);
+    neg_index_shuffled = randperm(neg_samples, neg_samples);
+%     imshow(cur_gt);
+
+    for j=1:pos_samples
+        cur_row = pos_rows(pos_index_shuffled(j));
+        cur_col = pos_cols(pos_index_shuffled(j));
+        
+        % get patche s limits
+        rowstop = cur_row + 2 * feat_r;
+        colstop = cur_col + 2 * feat_r;
+        
+        % extract patch
+        cur_patch = channels(cur_row : rowstop, cur_col : colstop, :);
+        img_features(j, :) = ... 
+            reshape(cur_patch, 1, size(img_features, 2));
+    end
+    
+    for j=1:neg_samples
+        cur_row = neg_rows(neg_index_shuffled(j));
+        cur_col = neg_cols(neg_index_shuffled(j));
+        
+        % get patche s limits
+        rowstop = cur_row + 2 * feat_r;
+        colstop = cur_col + 2 * feat_r;
+        
+        % extract patch
+        cur_patch = channels(cur_row : rowstop, cur_col : colstop, :);
+        img_features(j + pos_samples, :) = ... 
+            reshape(cur_patch, 1, size(img_features, 2));
+    end
     
     % Fill in some of the rows of img_features. Don't worry about filling
     % in sketch_features initially.
