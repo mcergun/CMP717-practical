a4e3231f75a0ffdbf1ea93d129933f84054f27aa
Mert Can Ergün <mcergun@windowslive.com>
2017-03-08 21:37:26 +0300

    detect_sketch_tokens first proto
    patches are captured for each pixel and forestApply is called
    afterwards, dunno what to do next

--- detect_sketch_tokens.m
+++ detect_sketch_tokens.m
@@ -1,5 +1,6 @@
 function pb = detect_sketch_tokens(img, forest, feature_params)
 
+
 % 'img' is a test image.
 % 'forest' is the structure returned by 'forestTrain'.
 
@@ -11,15 +12,35 @@ function pb = detect_sketch_tokens(img, forest, feature_params)
 %decided to use the DAISY descriptor as an image feature, which might be a
 %decent idea).
 
+% feature params 
+feat_r = feature_params.CR;
+daisy_r = feature_params.R;
+feat_sz = 2 * feat_r + 1;
+
 [height, width, cc] = size(img);
-pb  = zeros(height, width);
+pb = zeros(height, width);
 num_sketch_tokens = max(forest(1).hs) - 1; %-1 for background class
 
 % Pad the current image and then call 'channels = get_channels(cur_img)'
+img_padded = imPad(img, feat_r, 'symmetric');
+channels = get_channels(img_padded);
+patches = zeros(width * height, feat_sz * feat_sz * 14);
+for cur_row = 1:height
+    row_stop = cur_row + 2 * feat_r;
+    row_offset = (cur_row - 1) * width;
+    for cur_col = 1:width
+        col_stop = cur_col + 2 * feat_r;
+        patches(row_offset + cur_col, :) = ...
+            reshape(channels(cur_row:row_stop, cur_col:col_stop, :), 1, ...
+            size(patches));
+    end
+end
 
+patches = single(patches);
 % Stack all of the image features into one matrix. This will be redundant
 % (a single pixel will appear in many patches) but it will be faster than
 % calling 'forestApply' for every single pixel.
+[categories, probs] = forestApply(patches,forest);
 
 % Call 'forestApply', use the resulting probabilities to build the output
 % 'pb'
