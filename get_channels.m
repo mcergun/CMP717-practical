d5db15a75bb2d74a484809fc98f2ec9a6094fa74
Mert Can Ergün <mcergun@windowslive.com>
2017-03-12 22:22:40 +0300

    new get_sketch_tokens with post gaussian filtering

--- get_channels.m
+++ get_channels.m
@@ -3,6 +3,8 @@ function channels = get_channels(img)
 %'channels' is height x width x 14, with the 14 channels specified in
 %sketch tokens Section 2.2.1
 
+post_gaussian = fspecial('gaussian', [5 5], 1);
+
 % helpful functions: I = rgbConvert(I,'luv') and imfilter
 rows = size(img, 1);
 cols = size(img, 2);
@@ -12,6 +14,9 @@ channels = zeros(rows, cols, 14);
 
 % get LUV channels
 channels(:, :, 1:3) = rgbConvert(img, 'luv');
+
+for i=1:3
+    channels(:, :, i) = imfilter(channels(:, :, i), post_gaussian, 'symmetric');
 
 img_gray = rgb2gray(img);
 
@@ -34,15 +39,17 @@ for i=1:3
     idx = imfilter(img_gray, hx, 'symmetric'); 
     idy = imfilter(img_gray, hy, 'symmetric'); 
     
-    channels(:, :, i + 3) = hypot(idx, idy);
+    channels(:, :, i + 3) = imfilter(hypot(idx, idy), post_gaussian, 'symmetric');
     % if sigma = 0, 1.5
     if i < 3
         start_index = 7+4*(i-1);
         stop_index = 6+4*i;
         for j = start_index:stop_index
             angle = angles(j-6-4*(i-1));
-            channels(:,:, j) = hypot(idx.*cos(angle), idy.*sin(angle));
+            channels(:,:, j) = imfilter(hypot(idx.*cos(angle), idy.*sin(angle)), ...
+                post_gaussian, 'symmetric');
         end
     end
 end
 end
+
