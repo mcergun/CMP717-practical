6af7f1dc60c9d33e1ca4965d100650e0451ec648
Mert Can Ergün <mcergun@windowslive.com>
2017-03-12 22:21:21 +0300

    updated as there is no longer a get_channels2 function

--- Untitled.m
+++ Untitled.m
@@ -8,21 +8,13 @@ file_name = '196027.jpg';
 img = im2double(imread(strcat(location, file_name)));
 
 channels = get_channels(img);
-channels2 = get_channels2(img);
 
-figure, subplot(2, 3, 1)
+figure, subplot(1, 3, 1)
 imshow(channels(:, :, 4))
-subplot(2, 3, 2)
+subplot(1, 3, 2)
 imshow(channels(:, :, 5))
-subplot(2, 3, 3)
+subplot(1, 3, 3)
 imshow(channels(:, :, 6))
-
-subplot(2, 3, 4)
-imshow(channels2(:, :, 4))
-subplot(2, 3, 5)
-imshow(channels2(:, :, 5))
-subplot(2, 3, 6)
-imshow(channels2(:, :, 6))
 
 figure, subplot(2, 4, 1)
 imshow(channels(:, :, 7))
@@ -41,22 +33,4 @@ subplot(2, 4, 7)
 imshow(channels(:, :, 13))
 subplot(2, 4, 8)
 imshow(channels(:, :, 14))
-
-figure, subplot(2, 4, 1)
-imshow(channels2(:, :, 7))
-subplot(2, 4, 2)
-imshow(channels2(:, :, 8))
-subplot(2, 4, 3)
-imshow(channels2(:, :, 9))
-subplot(2, 4, 4)
-imshow(channels2(:, :, 10))
-
-subplot(2, 4, 5)
-imshow(channels2(:, :, 11))
-subplot(2, 4, 6)
-imshow(channels2(:, :, 12))
-subplot(2, 4, 7)
-imshow(channels2(:, :, 13))
-subplot(2, 4, 8)
-imshow(channels2(:, :, 14))
 
